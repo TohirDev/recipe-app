bdcb1566f2d039bc3c81c8e0c12e493e8b4d45ba
Isakulov Tohir <isakulovtohir902@gmail.com>
2022-07-18 17:40:57 +0500

    changer

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const API_KEY = '9beea8bdee1e48f5809bf2e5cc412f09'
 const API_URL = `https://api.spoonacular.com/recipes/random?&apiKey=${API_KEY}`
 
 fetch(API_URL)
-    .then(res => res.json())
     .then(foods => {
             document.body.innerHTML += `
             <div class="card">
